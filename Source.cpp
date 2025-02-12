97bb4a763d2f2a5de1a7fe3de8070b8cc1cad89b
627719@eastridingcollege.ac.uk <627719@eastridingcollege.ac.uk>
2025-02-12 19:38:40 +0000

    Add project files.

---
+++ Source.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Windows.h"
+
+DWORD GetProcId(const char* procName)
+{
+	HWND acWindow = FindWindow(nullptr, "AssaultCube");
+
+	DWORD processId;
+	GetWindowThreadProcessId(acWindow, &processId);
+
+	return processId;
+}
+
+int main()
+{
+	const char* dllPath = "C://dev//learning/acDLL/Debug//acDll.dll";
+	const char* procName = "ac_client.exe";
+
+	DWORD processId = GetProcId(procName);
+
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+
+	void* spaceForDllPath = VirtualAllocEx(hProc, nullptr, strlen(dllPath), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+
+	WriteProcessMemory(hProc, spaceForDllPath, dllPath, strlen(dllPath), nullptr);
+
+	HANDLE hThread = CreateRemoteThread(hProc, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA, spaceForDllPath, NULL, nullptr);
+
+	CloseHandle(hThread);
+	CloseHandle(hProc);
+
+
+
+
+}
