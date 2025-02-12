3424d25bdc25eff30ffa821f610064b0b912055e
627719@eastridingcollege.ac.uk <627719@eastridingcollege.ac.uk>
2025-02-12 22:32:44 +0000

    Added the comments to the code way easier to read

--- Source.cpp
+++ Source.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
 #include "Windows.h"
 
-DWORD GetProcId(const char* procName)
+DWORD GetProcId(const char* procName) // Expects  a return of a DWORD
 {
-	HWND acWindow = FindWindow(nullptr, "AssaultCube");
+	HWND acWindow = FindWindow(nullptr, "AssaultCube"); // HWIND = Handel to window, Its just a number that points to a place in memory, FindWindow(class, Name of the window)
 
-	DWORD processId;
-	GetWindowThreadProcessId(acWindow, &processId);
+	DWORD processId; // declaring it
+	GetWindowThreadProcessId(acWindow, &processId); // feeding it the handel to the window -> returns the process ID -> returns it to the address of processID
 
-	return processId;
+	return processId; // return process ID, 
 }
 
 int main()
 {
-	const char* dllPath = "C://dev//learning/acDLL/Debug//acDll.dll";
-	const char* procName = "ac_client.exe";
+	const char* dllPath = "C://dev//learning/acDLL/Debug//acDll.dll"; // Assigning a constant char pointer to our dllPath, constant because the location of this file will never change (if it does we come back and change)
+	const char* procName = "ac_client.exe"; // procname explains itself
 
-	DWORD processId = GetProcId(procName);
+	DWORD processId = GetProcId(procName); // Windows stores processs ID'S as DWORDS so we match that, processId gets its value from GetProcId function on line 4
 
-	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId);
+	HANDLE hProc = OpenProcess(PROCESS_ALL_ACCESS, NULL, processId); // Complicated so check OneNote
 
-	void* spaceForDllPath = VirtualAllocEx(hProc, nullptr, strlen(dllPath), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	void* spaceForDllPath = VirtualAllocEx(hProc, nullptr, strlen(dllPath), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE); // Again on one Note
 
-	WriteProcessMemory(hProc, spaceForDllPath, dllPath, strlen(dllPath), nullptr);
+	WriteProcessMemory(hProc, spaceForDllPath, dllPath, strlen(dllPath), nullptr); //One Note
 
-	HANDLE hThread = CreateRemoteThread(hProc, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA, spaceForDllPath, NULL, nullptr);
+	HANDLE hThread = CreateRemoteThread(hProc, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryA, spaceForDllPath, NULL, nullptr);// One note
 
-	CloseHandle(hThread);
+	CloseHandle(hThread); // closes the threads
 	CloseHandle(hProc);
 
 
